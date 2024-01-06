83d218ec6e8ab455aa433675101ba58a619d87ae
izznazrin <nazrin.izryzza@gmail.com>
2024-01-06 18:11:36 +0800

    6/1/2024

---
+++ src/main/java/com/izznazrin/aplikasi/AplikasiApplication.java
@@ -0,0 +1,13 @@
+package com.izznazrin.aplikasi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/izznazrin/aplikasi/AplikasiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.izznazrin.aplikasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikasiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

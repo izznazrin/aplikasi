051fd1a67abbadfe593ccc0556bad22b5feee659
izznazrin <nazrin.izryzza@gmail.com>
2024-01-07 04:26:21 +0800

    7/1/2023

--- src/main/java/com/izznazrin/Customer.java
+++ src/main/java/com/izznazrin/Customer.java
@@ -1,6 +1,22 @@
 package com.izznazrin;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+@Entity
 public class Customer {
+
+    @Id
+    @SequenceGenerator(
+        name = "customer_id_sequence",
+        sequenceName = "customer_id_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "customer_id_sequence"
+    )
     private Integer id;
     private String name;
     private String email;
--- src/main/java/com/izznazrin/Main.java
+++ src/main/java/com/izznazrin/Main.java
@@ -2,35 +2,49 @@ package com.izznazrin;
 
 import java.util.List;
 
-import org.springframework.beans.factory.config.ListFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/test")
-    public GreetResponse greet() {
-        GreetResponse response = new GreetResponse(
-            "hell",
-            List.of("Java", "JavaScript"),
-            new Person("Alex", 28, 30000)
-        );
-        return response;
+    @GetMapping()
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
     }
 
-    record Person(String name, int age, double savings) {
-    }
+    // @GetMapping("/test")
+    // public GreetResponse greet() {
+    //     GreetResponse response = new GreetResponse(
+    //         "hell",
+    //         List.of("Java", "JavaScript"),
+    //         new Person("Alex", 28, 30000)
+    //     );
+    //     return response;
+    // }
 
-    record GreetResponse(
-            String greet,
-            List<String> favProgrammingLanguages,
-            Person person) {
-    }
+    // record Person(String name, int age, double savings) {
+    // }
+
+    // record GreetResponse(
+    //         String greet,
+    //         List<String> favProgrammingLanguages,
+    //         Person person) {
+    // }
 }
